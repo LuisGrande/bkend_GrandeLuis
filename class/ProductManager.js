a2ce444604a74a2fd2f8ad70df362275b9059956
Luis Grande <grandel.programador@gmail.com>
2023-03-01 22:48:56 -0300

    proyecto segunda entrega

--- class/ProductManager.js
+++ class/ProductManager.js
@@ -1,9 +1,31 @@
+const fs = require('fs')
+
 class ProductManager {
-  constructor() {
+  constructor(path) {
+    this.path = path
     this.products = [];
+    this.loadProducts()
   }
 
   static id = 000;
+
+  loadProducts(){
+    try {
+      const data = fs.readFileSync(this.path,'utf-8')
+      this.products = JSON.parse(data)
+    } catch (err) {
+      console.log(`error: ${err}`)
+    }
+  }
+
+  saveProducts() {
+    try {
+      const data = JSON.stringify(this.products);
+      fs.writeFileSync(this.path, data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   addProduct(title, description, price, thumbnail, code, stock) {
      const productoBuscado = this.products.find((prod) => prod.code === code);
@@ -37,6 +59,21 @@ class ProductManager {
     } else {
       console.log("no se encontro el producto");
     }
+  }
+  
+  updateProduct(id, updatedFields) {
+    const index = this.products.findIndex(p => p.id === id);
+    if (index !== -1) {
+      const updatedProduct = {
+        ...this.products[index],
+        ...updatedFields,
+        id // aseguramos que el ID del producto no cambie
+      };
+      this.products[index] = updatedProduct;
+      this.saveProducts();
+      return updatedProduct;
+    }
+    return null;
   }
 }
 
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const ProductManager = require("./class/ProductManager")
 
 // instancio la clase
-const product = new ProductManager
+const product = new ProductManager('./productos.txt')
 // agrego productos
-product.addProduct('pantalon','levis desgastado',13000,'direccion_pantalon','01A',24)
+// product.addProduct('pantalon','levis desgastado',13000,'direccion_pantalon','01A',24)
+// product.saveProducts()
 product.addProduct('remera','indumentaria de verano',3000,'direccion','01B',30)
-product.addProduct('remera','indumentaria de verano',3000,'direccion','01B',30)
-console.log(product.getProduct())
-product.getProductById(1)
+product.saveProducts()
+// ingreso un producto con codigo repetido
+// product.addProduct('remera','indumentaria de verano',3000,'direccion','01B',30)
+// console.log(product.getProduct())
+// product.getProductById(1)
+
+
